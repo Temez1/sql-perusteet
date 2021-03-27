1baed22e757de2276de32624018919d705cfa4a7
Teemu Helenius <heleniusteemu@gmail.com>
2021-03-27 13:15:20 +0200

    Split H1 create-db and create-data to own files

---
+++ H1/create-data.sql
@@ -0,0 +1,8 @@
+INSERT INTO movie
+VALUES ('Jaws', 'Spielberg', '1975-12-19');
+INSERT INTO movie
+VALUES ('Jurassic Park', 'Spielberg', '1993-09-03');
+INSERT INTO movie
+VALUES ('2001: A Space Odyssey', 'Kubrick', '1968-09-20');
+INSERT INTO movie
+VALUES ('The Shining', 'Kubrick', '1980-09-26');
--- H1/create-db.sql
+++ H1/create-db.sql
@@ -4,16 +4,3 @@ CREATE TABLE movie (
     release_date date,
     PRIMARY KEY (title)
 );
-
-
-INSERT INTO movie
-VALUES ('Jaws', 'Spielberg', '1975-12-19');  
-
-INSERT INTO movie
-VALUES ('Jurassic Park', 'Spielberg', '1993-09-03');  
-
-INSERT INTO movie
-VALUES ('2001: A Space Odyssey', 'Kubrick', '1968-09-20');
-
-INSERT INTO movie
-VALUES ('The Shining', 'Kubrick', '1980-09-26');  
